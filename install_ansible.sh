63ba110ab27591e6bb9f83f90e70b8a341a28575
Damian Kelly <damian.t.kelly@gmail.com>
2017-03-31 15:35:34 +0100

    Temporarily using ansible 2.1 to compensate for a recent bug in ansible; https://github.com/ansible/ansible/issues/23016.

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,4 +1,4 @@
 sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-add-repository ppa:ansible/ansible-2.1 -y
 sudo apt-get update
 sudo apt-get install ansible -y
